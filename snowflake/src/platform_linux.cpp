28966dc6bd1630d79d584c2a624031bbfe67f4dd
Dimitry <73913361+Dimitry-bit@users.noreply.github.com>
2023-06-26 00:06:33 +0300

    Add: Platform console write

---
+++ snowflake/src/platform_linux.cpp
@@ -0,0 +1,22 @@
+#include "platform.h"
+#include "defines.h"
+
+#if SPLATFORM_LINUX
+
+#include <cstdio>
+
+void PlatformConsoleWrite(const char* msg, u8 color)
+{
+    // TRACE,DEBUG,INFO,WARN,ERROR,FATAL
+    const char* colorStrs[] = { 0, "1;30", "1;36", "1;32", "1;33", "1;31", "0;41"};
+    fprintf(stdout, "\033[%sm%s\033[0m", colorStrs[color], msg);
+}
+
+void PlatformConsoleWriteError(const char* msg, u8 color)
+{
+    // TRACE,DEBUG,INFO,WARN,ERROR,FATAL
+    const char* colorStrs[] = { 0, "1;30", "1;36", "1;32", "1;33", "1;31", "0;41"};
+    fprintf(stderr, "\033[%sm%s\033[0m", colorStrs[color], msg);
+}
+
+#endif
---
+++ snowflake/src/platform_win32.cpp
@@ -0,0 +1,32 @@
+#include "platform.h"
+#include "defines.h"
+
+#if SPLATFORM_WINDOWS
+
+#include "windows.h"
+
+void PlatformConsoleWrite(const char* message, u8 color)
+{
+    // TRACE,DEBUG,INFO,WARN,ERROR,FATAL
+    static u8 levels[] = {0, 8, 3, 2, 6, 4, 64};
+    HANDLE console_handle = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(console_handle, levels[color]);
+    OutputDebugStringA(message);
+    size_t length = strlen(message);
+    DWORD number_written = 0;
+    WriteConsoleA(GetStdHandle(STD_OUTPUT_HANDLE), message, (DWORD) length, &number_written, 0);
+}
+
+void PlatformConsoleWriteError(const char* message, u8 color)
+{
+    // TRACE,DEBUG,INFO,WARN,ERROR,FATAL
+    static u8 levels[] = {0, 8, 3, 2, 6, 4, 64};
+    HANDLE console_handle = GetStdHandle(STD_ERROR_HANDLE);
+    SetConsoleTextAttribute(console_handle, levels[color]);
+    OutputDebugStringA(message);
+    size_t length = strlen(message);
+    DWORD number_written = 0;
+    WriteConsoleA(GetStdHandle(STD_ERROR_HANDLE), message, (DWORD) length, &number_written, 0);
+}
+
+#endif
