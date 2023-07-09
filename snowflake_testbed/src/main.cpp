4862aaed3013b6202227781acf48b06eefdbf63a
Dimitry <73913361+Dimitry-bit@users.noreply.github.com>
2023-07-09 19:19:31 +0300

    Refator: testbed main.cpp

--- snowflake_testbed/src/main.cpp
+++ snowflake_testbed/src/main.cpp
@@ -5,6 +5,12 @@
 #include "srenderer_internal.h"
 #include "smemory.h"
 
+static void TestWindow(i32 configFlags);
+static void TestMathFunctions();
+static void TestFileFunctions();
+static void TestInput();
+static void TestPrimitiveShapes();
+
 int main()
 {
     i32 configFlags = FLAG_WINDOW_RESIZEABLE | FLAG_CONTEXT_OPENGL_3 | FLAG_CONTEXT_OPENGL_CORE_PROFILE;
@@ -13,6 +19,44 @@ int main()
         return -1;
     }
 
+    TestWindow(configFlags);
+    TestMathFunctions();
+    TestFileFunctions();
+
+    Texture2D tex = TextureLoadFromFile("../resources/wall.bmp");
+
+    LOG_INFO(SMemUsage());
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(SNOWWHITE);
+
+        {
+            const u32 titleLen = 512;
+            char title[titleLen] = { };
+            snprintf(title, titleLen, "WinPos:(X:%g, Y:%g) | MPos:(X:%g, Y:%g) | FTime:'%.1f'ms | FPS:%u",
+                     GetWindowPosition().x, GetWindowPosition().y,
+                     GetMousePosition().x, GetMousePosition().y,
+                     GetFrameTime() * 1000, GetFPS());
+            SetWindowTitle(title);
+        }
+
+        TestInput();
+        TestPrimitiveShapes();
+
+        EndDrawing();
+        PollInputEvents();
+    }
+
+    TextureDelete(&tex);
+
+    CloseWindow();
+
+    return 0;
+}
+
+static void TestWindow(i32 configFlags)
+{
     SASSERT(IsWindowState(configFlags) == true);
 
     LOG_INFO("Screen Size: %dx%d", (i32) GetWindowSize().x, (i32) GetWindowSize().y);
@@ -38,6 +82,10 @@ int main()
     DisableCursor();
     EnableCursor();
 
+}
+
+static void TestMathFunctions()
+{
     SASSERT(Min(5, 10) == 5);
     SASSERT(Min(5.0f, 10.0f) == 5.0f);
     SASSERT(Max(5, 10) == 10);
@@ -50,9 +98,10 @@ int main()
     SASSERT(Abs(-2.0f) == 2.0f);
     SASSERT(Square(-2) == 4);
     SASSERT(Square(-2.0f) == 4.0f);
+}
 
-    Texture2D tex = TextureLoadFromFile("../resources/wall.bmp");
-
+static void TestFileFunctions()
+{
     StringViewer fn = FileGetFileName("../resources/wall.bmp");
     StringViewer fe = FileGetExtension("../resources/wall.bmp");
     StringViewer fne = FileGetFileNameNoExtension("../resources/wall.bmp");
@@ -64,99 +113,72 @@ int main()
     SASSERT(strncmp(path.data, "../resources/", path.length) == 0 && path.length == 13);
     LOG_DEBUG("FileName '%.*s', Extension '%.*s', FileNameOnly '%.*s', Path '%.*s'",
               fn.length, fn.data, fe.length, fe.data, fne.length, fne.data, path.length, path.data);
+}
 
-    LOG_INFO(SMemUsage());
-
-    while (!WindowShouldClose()) {
-        BeginDrawing();
-        ClearBackground(OLDBLACK);
-
-        {
-            const u32 titleLen = 512;
-            char title[titleLen] = { };
-            snprintf(title, titleLen, "WinPos:(X:%g, Y:%g) | MPos:(X:%g, Y:%g) | FTime:'%.1f'ms | FPS:%u",
-                     GetWindowPosition().x, GetWindowPosition().y,
-                     GetMousePosition().x, GetMousePosition().y,
-                     GetFrameTime() * 1000, GetFPS());
-            SetWindowTitle(title);
-        }
-
-        {
-            if (IsKeyPressed(KEY_1)) {
-                RendererSetPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-                LOG_DEBUG("'%d' Key is Pressed", KEY_1);
-            }
-            if (IsKeyPressed(KEY_2)) {
-                RendererSetPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-            }
-            if (IsKeyPressed(KEY_3)) {
-                RendererSetPolygonMode(GL_FRONT_AND_BACK, GL_POINT);
-            }
-            if (IsKeyReleased(KEY_1)) {
-                LOG_DEBUG("'%d' Key is Released", KEY_1);
-            }
-
-            if (IsMousePressed(MOUSE_BUTTON_LEFT)) {
-                LOG_DEBUG("'%d' Key is Pressed", MOUSE_BUTTON_LEFT);
-            }
-            if (IsMouseReleased(MOUSE_BUTTON_LEFT)) {
-                LOG_DEBUG("'%d' Key is Released", MOUSE_BUTTON_LEFT);
-            }
-            if (GetMouseWheel().y > 0 || GetMouseWheel().y < 0) {
-                LOG_DEBUG("MouseWheel move %f", GetMouseWheel().y);
-            }
-        }
-
-        {
-            {
-                f32 width = 50.0f;
-                f32 height = 50.0f;
-                f32 padding = 20.0f;
-                i32 countX = (GetWindowWidth() - (padding * 2)) / width;
-                i32 countY = (GetWindowHeight() - (padding * 2)) / height;
-                for (i32 i = 0; i < countX; ++i) {
-                    for (i32 j = 0; j < countY; ++j) {
-                        f32 posX = padding + i * (width + padding);
-                        f32 posY = padding + j * (height + padding);
-                        Sprite sprite = SpriteCreate(posX, posY, width, height);
-                        sprite.transform.rotation = Sin((f32) GetTime());
-                        sprite.transform.origin = Vec2{ 0.5f * sprite.width, 0.5f * sprite.height };
-                        sprite.texture = &tex;
-                        DrawSprite(&sprite);
-                    }
-                }
-            }
-
-            f32 scrWidth = GetWindowWidth();
-            f32 scrHeight = GetWindowHeight();
-
-            Vec2 v1 = { scrWidth / 2.0f, scrHeight / 2.0f - 100.0f };
-            Vec2 v2 = { scrWidth / 2.0f - 100.0f, scrHeight / 2.0f + 100.0f };
-            Vec2 v3 = { scrWidth / 2.0f + 100.0f, scrHeight / 2.0f + 100.0f };
-
-            DrawPixel(scrWidth - 100.0f, 50.0f, WHITE);
-            DrawPixel(scrWidth - 150.0f, 50.0f, WHITE);
-            DrawLine(500.0f, 50.0f, 600.0f, 50.0f, 1.0f, DEEPMAGENTA);
-
-            CircleShape circle = CircleCreate(0.0f, 0.0f, 40.0f, GLOSSYCYAN);
-            DrawCirclePro(&circle);
-
-            EllipseShape ellipse = EllipseCreate(100.0f, 0.0f, 20.0f, 40.0f, GOLD);
-            DrawEllipsePro(&ellipse);
-
-            RingShape ring = RingCreate(200.0f, 0.0f, 35.0f, 40.0f, COSMICPINK);
-            DrawRingPro(&ring);
-
-            DrawTriangle(v1, v2, v3, CANDYPURPLE);
-        }
-
-        EndDrawing();
-        PollInputEvents();
+static void TestInput()
+{
+    if (IsKeyPressed(KEY_1)) {
+        RendererSetPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+        LOG_DEBUG("'%d' Key is Pressed", KEY_1);
+    }
+    if (IsKeyPressed(KEY_2)) {
+        RendererSetPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    }
+    if (IsKeyPressed(KEY_3)) {
+        RendererSetPolygonMode(GL_FRONT_AND_BACK, GL_POINT);
+    }
+    if (IsKeyReleased(KEY_1)) {
+        LOG_DEBUG("'%d' Key is Released", KEY_1);
     }
 
-    TextureDelete(&tex);
+    if (IsMousePressed(MOUSE_BUTTON_LEFT)) {
+        LOG_DEBUG("'%d' Key is Pressed", MOUSE_BUTTON_LEFT);
+    }
+    if (IsMouseReleased(MOUSE_BUTTON_LEFT)) {
+        LOG_DEBUG("'%d' Key is Released", MOUSE_BUTTON_LEFT);
+    }
+    if (GetMouseWheel().y > 0 || GetMouseWheel().y < 0) {
+        LOG_DEBUG("MouseWheel move %f", GetMouseWheel().y);
+    }
+}
 
-    CloseWindow();
+static void TestPrimitiveShapes()
+{
+    f32 width = 50.0f;
+    f32 height = 50.0f;
+    f32 padding = 20.0f;
+    i32 countX = (GetWindowWidth() - (padding * 2)) / width;
+    i32 countY = (GetWindowHeight() - (padding * 2)) / height;
+    for (i32 i = 0; i < countX; ++i) {
+        for (i32 j = 0; j < countY; ++j) {
+            f32 posX = padding + i * (width + padding);
+            f32 posY = padding + j * (height + padding);
+            Sprite sprite = SpriteCreate(posX, posY, width, height, ANGLEBLUE);
+            sprite.transform.rotation = Sin((f32) GetTime());
+            sprite.transform.origin = Vec2{ 0.5f * sprite.width, 0.5f * sprite.height };
+            DrawSprite(&sprite);
+        }
+    }
 
-    return 0;
+    f32 scrWidth = GetWindowWidth();
+    f32 scrHeight = GetWindowHeight();
+
+    Vec2 v1 = { scrWidth / 2.0f, scrHeight / 2.0f - 100.0f };
+    Vec2 v2 = { scrWidth / 2.0f - 100.0f, scrHeight / 2.0f + 100.0f };
+    Vec2 v3 = { scrWidth / 2.0f + 100.0f, scrHeight / 2.0f + 100.0f };
+
+    DrawPixel(scrWidth - 100.0f, 50.0f, WHITE);
+    DrawPixel(scrWidth - 150.0f, 50.0f, WHITE);
+    DrawLine(500.0f, 50.0f, 600.0f, 50.0f, 1.0f, DEEPMAGENTA);
+
+    CircleShape circle = CircleCreate(0.0f, 0.0f, 40.0f, GLOSSYCYAN);
+    DrawCirclePro(&circle);
+
+    EllipseShape ellipse = EllipseCreate(100.0f, 0.0f, 20.0f, 40.0f, GOLD);
+    DrawEllipsePro(&ellipse);
+
+    RingShape ring = RingCreate(200.0f, 0.0f, 35.0f, 40.0f, COSMICPINK);
+    DrawRingPro(&ring);
+
+    DrawTriangle(v1, v2, v3, CANDYPURPLE);
 }
